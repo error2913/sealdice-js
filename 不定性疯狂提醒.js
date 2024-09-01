40d06d55039b2ca0f2327abfe8deb03f3f359b29
error2913 <2913949387@qq.com>
2024-09-01 19:05:52 +0800

    Update 不定性疯狂提醒.js
    修改了一下回复的配置

--- 不定性疯狂提醒.js
+++ 不定性疯狂提醒.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         不定性疯狂提醒
 // @author       错误
-// @version      1.0.0
+// @version      1.0.1
 // @description  通过指令sc，st来进行检测。需要手动录入一个msan属性作为san值上限（不录入默认等于意志），因为没有办法知道团内“一天”什么时候开始。
 // @timestamp    1725024341
 // 2024-08-30 21:25:41
@@ -11,12 +11,12 @@
 // ==/UserScript==
 let ext = seal.ext.find('crazycheck');
 if (!ext) {
-    ext = seal.ext.new('crazycheck', '错误', '1.0.0');
+    ext = seal.ext.new('crazycheck', '错误', '1.0.1');
     // 注册扩展
     seal.ext.register(ext);
     seal.ext.registerBoolConfig(ext, '是否在log开启时提醒录入san上限', true)
     seal.ext.registerStringConfig(ext, 'log开启时提醒录入san上限', '为了能自动提醒不定性疯狂，当“一天”开始时，请记得录入“一天”开始时san值上限，指令.st msan数字。不录入默认等于意志。')
-    seal.ext.registerStringConfig(ext, '不定性疯狂提醒', '已不定性疯狂')
+    seal.ext.registerStringConfig(ext, '不定性疯狂提醒', '<{{调查员}}>已不定性疯狂')
 
     const cmdlog = seal.ext.newCmdItemInfo();
     cmdlog.name = 'log'; // 指令名字，可用中文
@@ -43,7 +43,7 @@ if (!ext) {
     cmdsc.help = '';
     cmdsc.allowDelegate = true;
     cmdsc.solve = (ctx, msg, cmdArgs) => {
-        if (ctx.group.logOn) setTimeout(() => {if (checksan(ctx)) seal.replyToSender(ctx, msg, seal.ext.getStringConfig(ext, '不定性疯狂提醒'));}, 500)
+        if (ctx.group.logOn) setTimeout(() => {if (checksan(ctx)) seal.replyToSender(ctx, msg, seal.ext.getStringConfig(ext, '不定性疯狂提醒').replace('{{调查员}}', ctx.player.name));}, 500)
     };
     const cmdst = seal.ext.newCmdItemInfo();
     cmdst.name = 'st'; // 指令名字，可用中文
@@ -51,7 +51,7 @@ if (!ext) {
     cmdst.allowDelegate = true;
     cmdst.solve = (ctx, msg, cmdArgs) => {
         let val = cmdArgs.getRestArgsFrom(1)
-        if (/san/.test(val)) if (ctx.group.logOn) setTimeout(() => {if (checksan(ctx)) seal.replyToSender(ctx, msg, seal.ext.getStringConfig(ext, '不定性疯狂提醒'));}, 500)
+        if (/san/.test(val)) if (ctx.group.logOn) setTimeout(() => {if (checksan(ctx)) seal.replyToSender(ctx, msg, seal.ext.getStringConfig(ext, '不定性疯狂提醒').replace('{{调查员}}', ctx.player.name));}, 500)
     };
 
     // 将命令注册到扩展中
