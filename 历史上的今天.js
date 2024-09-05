4ba3880e596ba4586e71efd60a145cfc5f1a20e1
error2913 <2913949387@qq.com>
2024-09-05 16:23:13 +0800

    Create 历史上的今天.js

---
+++ 历史上的今天.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @name         历史上的今天
+// @author       错误
+// @version      1.0.0
+// @description  发送‘历史上的今天’，获取历史上的今天的事件
+// @timestamp    1725522223
+// 2024-09-05 15:43:43
+// @license      MIT
+// @homepageURL  https://github.com/sealdice/javascript
+// ==/UserScript==
+
+// 首先检查是否已经存在
+let ext = seal.ext.find('historytoday');
+if (!ext) {
+    ext = seal.ext.new('historytoday', '错误', '1.0.0');
+    seal.ext.register(ext);
+
+    const apiUrl = 'https://tenapi.cn/v2/history';
+    const cache = {date:'' ,eventlist: []}
+
+    ext.onNotCommandReceived = async (ctx, msg) => {
+        if (msg.message == '历史上的今天') {
+            let eventList = []
+            let date = seal.format(ctx, `{$tDate}`)
+            if (date == cache.date) eventList = cache.eventlist
+            else {
+                try {
+                    // 使用fetch发送GET请求
+                    const response = await fetch(apiUrl);
+                    // 检查响应是否成功
+                    if (!response.ok) {
+                        throw new Error('网络响应失败');
+                    }
+                    // 将响应转换为JSON
+                    const data = await response.json();
+
+                    eventList = data.data.list
+                    cache.date = date
+                    cache.eventlist = eventList
+                } catch (error) {
+                    // 处理错误
+                    console.error('请求失败:', error);
+                }
+            }
+            if (eventList.length > 0) {
+                let event = eventList[Math.floor(Math.random() * eventList.length)]
+                seal.replyToSender(ctx, msg, `${event.title}(${event.year})`)
+            }
+            return;
+        }
+    }
+}
