03c2e00edd7a301e2a8d5ba574eef4ca364c09d0
error2913 <2913949387@qq.com>
2024-09-16 00:15:26 +0800

    Update 调查员队伍.js

--- 调查员队伍.js
+++ 调查员队伍.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         team call
 // @author       错误
-// @version      2.0.1
+// @version      2.0.2
 // @description  .team 获取帮助。在其他框架看到类似的插件，找了一下发现海豹似乎没有，故自己写一个。使用.team获取帮助。非指令关键词部分请查看插件配置。
 // @timestamp    1724468302
 // 2024-08-24 10:58:22
@@ -13,7 +13,7 @@
 // 首先检查是否已经存在
 let ext = seal.ext.find('team call');
 if (!ext) {
-    ext = seal.ext.new('team call', '错误', '2.0.1');
+    ext = seal.ext.new('team call', '错误', '2.0.2');
     seal.ext.register(ext);
     const data = {}
 
@@ -78,16 +78,18 @@ if (!ext) {
     cmdteam.name = 'team';
     cmdteam.help = `帮助：
 【.team new 队伍名字】新建一个队伍
-【.team clr 队伍名字/now】删除该队伍
-【.team tag 队伍名字】绑定该队伍
+【.team del 队伍名字/now】删除该队伍
+【.team bind 队伍名字】绑定该队伍
 【.team lst】队伍列表
 【.team add (me)@xx@xxx...】添加若干成员
-【.team del (me)@xx@xxx...】删除若干成员
+【.team rm (me)@xx@xxx...】删除若干成员
+【.team clr】删除所有成员
 【.team call】调查员集结！
 【.team show】成员列表
-【.team st (属性名/表达式)】修改全队属性
+【.team st (属性名/表达式)】查看属性(修改属性)
 【.team sort 属性名】对成员的该项属性排序`;
     cmdteam.allowDelegate = true;
+    cmdteam.disabledInPrivate = true;
     cmdteam.solve = (ctx, msg, cmdArgs) => {
         if (ctx.isPrivate) return;
 
@@ -97,13 +99,10 @@ if (!ext) {
         if (!data.hasOwnProperty(groupId)) getData(groupId)
         let teamnow = data[groupId].teamnow
 
-
-        if (val2 == 'me') atlst.push([ctx.endPoint.userId, groupId, msg.guildId, ctx.player.userId])
         //获取所有被@的人
         let atlst = []
-        cmdArgs.at.forEach(item => {
-            if (item.userId != ctx.endPoint.userId) atlst.push([ctx.endPoint.userId, groupId, msg.guildId, item.userId]);
-        });
+        if (val2 == 'me') atlst.push(ctx.player.userId)
+        cmdArgs.at.forEach(item => { if (item.userId != ctx.endPoint.userId) atlst.push(item.userId); });
 
         switch (val) {
             case 'help': {
@@ -122,9 +121,9 @@ if (!ext) {
                 saveData(groupId)
                 return seal.ext.newCmdExecuteResult(true);
             }
-            case 'clr': {
+            case 'del': {
                 if (!val2) {
-                    seal.replyToSender(ctx, msg, "参数错误，【.team clr 队伍名字/now】删除该队伍")
+                    seal.replyToSender(ctx, msg, "参数错误，【.team del 队伍名字/now】删除该队伍")
                     return seal.ext.newCmdExecuteResult(true);
                 }
                 if (val2 == 'now') {
@@ -143,9 +142,9 @@ if (!ext) {
                 saveData(groupId)
                 return seal.ext.newCmdExecuteResult(true);
             }
-            case 'tag': {
+            case 'bind': {
                 if (!val2) {
-                    seal.replyToSender(ctx, msg, "参数错误，【.team tag 队伍名字】绑定该队伍")
+                    seal.replyToSender(ctx, msg, "参数错误，【.team bind 队伍名字】绑定该队伍")
                     return seal.ext.newCmdExecuteResult(true);
                 }
                 if (!data[groupId].teams.hasOwnProperty(val2)) {
@@ -173,16 +172,8 @@ if (!ext) {
                     return seal.ext.newCmdExecuteResult(true);
                 }
 
-                for (let mctxargs of atlst) {
-                    let sign = false
-                    for (let ctxargs of data[groupId].teams[teamnow]) {
-                        if (mctxargs[3] == ctxargs[3]) {
-                            sign = true
-                            break;
-                        }
-                    }
-                    if (!sign) data[groupId].teams[teamnow].push(mctxargs)
-                }
+                //添加进去
+                atlst.forEach(userId => { if (!data[groupId].teams[teamnow].includes(userId)) data[groupId].teams[teamnow].push(userId); });
 
                 let text = seal.ext.getStringConfig(ext, "添加成员回复")
                 text = text.replace('{{被@的长度}}', atlst.length)
@@ -191,18 +182,29 @@ if (!ext) {
                 saveData(groupId)
                 return seal.ext.newCmdExecuteResult(true);
             }
-            case 'del': {
+            case 'rm': {
                 if (!data[groupId].teams.hasOwnProperty(teamnow)) {
                     seal.replyToSender(ctx, msg, "未绑定队伍")
                     return seal.ext.newCmdExecuteResult(true);
                 }
 
-                data[groupId].teams[teamnow] = data[groupId].teams[teamnow].filter(ctxargs => !atlst.some(mctxargs => mctxargs[3] == ctxargs[3]))
+                data[groupId].teams[teamnow] = data[groupId].teams[teamnow].filter(userId => !atlst.includes(userId))
 
                 let text = seal.ext.getStringConfig(ext, "删除成员回复")
                 text = text.replace('{{被@的长度}}', atlst.length)
                 text = text.replace('{{当前人数}}', data[groupId].teams[teamnow].length)
                 seal.replyToSender(ctx, msg, text)
+                saveData(groupId)
+                return seal.ext.newCmdExecuteResult(true);
+            }
+            case 'clr': {
+                if (!data[groupId].teams.hasOwnProperty(teamnow)) {
+                    seal.replyToSender(ctx, msg, "未绑定队伍")
+                    return seal.ext.newCmdExecuteResult(true);
+                }
+
+                data[groupId].teams[teamnow] = []
+                seal.replyToSender(ctx, msg, "队伍已清空")
                 saveData(groupId)
                 return seal.ext.newCmdExecuteResult(true);
             }
@@ -218,7 +220,7 @@ if (!ext) {
 
                 let text = seal.ext.getStringConfig(ext, "呼叫队伍里所有成员前缀语")
                 text = text.replace('{{时间限制}}', seal.ext.getIntConfig(ext, "呼叫时间限制（s）"))
-                for (let mctxargs of data[groupId].teams[teamnow]) text += `\n[CQ:at,qq=${mctxargs[3].replace(/\D+/g, "")}]`;
+                for (let userId of data[groupId].teams[teamnow]) text += `\n[CQ:at,qq=${userId.replace(/\D+/g, "")}]`;
                 seal.replyToSender(ctx, msg, text)
 
                 data[groupId].call = data[groupId].teams[teamnow]
@@ -227,7 +229,7 @@ if (!ext) {
                     text = text.replace('{{当前人数}}', data[groupId].teams[teamnow].length)
                     text = text.replace('{{签到人数}}', data[groupId].teams[teamnow].length - data[groupId].call.length)
                     text = text.replace('{{咕咕人数}}', data[groupId].call.length)
-                    for (let mctxargs of data[groupId].call) text += `\n[CQ:at,qq=${mctxargs[3].replace(/\D+/g, "")}]`;
+                    for (let userId of data[groupId].call) text += `\n[CQ:at,qq=${userId.replace(/\D+/g, "")}]`;
                     seal.replyToSender(ctx, msg, text)
                     return;
                 }, seal.ext.getIntConfig(ext, "呼叫时间限制（s）") * 1000)
@@ -244,8 +246,8 @@ if (!ext) {
                 }
 
                 let text = seal.ext.getStringConfig(ext, "展示成员前缀语")
-                for (let mctxargs of data[groupId].teams[teamnow]) {
-                    let mctx = getCtxById(...mctxargs);
+                for (let userId of data[groupId].teams[teamnow]) {
+                    let mctx = getCtxById(ctx.endPoint.userId, groupId, msg.guildId, userId);
                     text += `\n${mctx.player.name}`;
                 }
                 seal.replyToSender(ctx, msg, text)
@@ -263,19 +265,20 @@ if (!ext) {
 
                 if (!val2) {
                     let text = seal.ext.getStringConfig(ext, "展示属性前缀语")
-                    for (let mctxargs of data[groupId].teams[teamnow]) {
-                        let mctx = getCtxById(...mctxargs);
+                    for (let userId of data[groupId].teams[teamnow]) {
+                        let mctx = getCtxById(ctx.endPoint.userId, groupId, msg.guildId, userId);
                         text += `\n${mctx.player.name} hp${seal.vars.intGet(mctx, 'hp')[0]}/${Math.floor((seal.vars.intGet(mctx, 'con')[0] + seal.vars.intGet(mctx, 'siz')[0]) / 10)} san${seal.vars.intGet(mctx, 'san')[0]}/${seal.vars.intGet(mctx, 'pow')[0]} dex${seal.vars.intGet(mctx, 'dex')[0]}`;
                     }
                     seal.replyToSender(ctx, msg, text)
                     return seal.ext.newCmdExecuteResult(true);
                 }
 
-                let match = val2.match(/^(.*?)([0-9+\-d]+)$/)
+                let match = val2.match(/^(.*?)([\d+\-*\/d]+)$/)
+                let text = seal.ext.getStringConfig(ext, "展示属性前缀语")
+
                 if (!match) {
-                    let text = seal.ext.getStringConfig(ext, "展示属性前缀语")
-                    for (let mctxargs of data[groupId].teams[teamnow]) {
-                        let mctx = getCtxById(...mctxargs);
+                    for (let userId of data[groupId].teams[teamnow]) {
+                        let mctx = getCtxById(ctx.endPoint.userId, groupId, msg.guildId, userId);
                         text += `\n${mctx.player.name} ${val2}${seal.vars.intGet(mctx, val2)[0]}`;
                     }
                     seal.replyToSender(ctx, msg, text)
@@ -283,26 +286,15 @@ if (!ext) {
                 }
                 let attr = match[1]
                 let expression = match[2]
-                let op = expression.match(/^([0-9+\-])/)[1]
-
-                if (op == '+' || op == '-') {
-                    let text = seal.ext.getStringConfig(ext, "展示属性前缀语")
-                    text += `\n操作:${val2}`
-                    for (let mctxargs of data[groupId].teams[teamnow]) {
-                        let mctx = getCtxById(...mctxargs);
-                        text += `\n${mctx.player.name} ${seal.vars.intGet(mctx, attr)[0]}=>${seal.format(mctx, `{${attr}=${val2}}`)}`
-                    }
-                    seal.replyToSender(ctx, msg, text)
-                    return seal.ext.newCmdExecuteResult(true);
-                } else {
-                    let text = seal.ext.getStringConfig(ext, "展示属性前缀语")
-                    for (let mctxargs of data[groupId].teams[teamnow]) {
-                        let mctx = getCtxById(...mctxargs);
-                        text += `\n${mctx.player.name} ${seal.vars.intGet(mctx, attr)[0]}=>${seal.format(mctx, `{${attr}=${expression}}`)}`
-                    }
-                    seal.replyToSender(ctx, msg, text)
-                    return seal.ext.newCmdExecuteResult(true);
+                let op = expression.match(/^([+\-])/)?.[1]
+                let result = op ? `{${attr}=${val2}}` : `{${attr}=${expression}}`
+                text += `\n操作:${val2}`
+                for (let userId of data[groupId].teams[teamnow]) {
+                    let mctx = getCtxById(ctx.endPoint.userId, groupId, msg.guildId, userId);
+                    text += `\n${mctx.player.name} ${seal.vars.intGet(mctx, attr)[0]}=>${seal.format(mctx, result)}`
                 }
+                seal.replyToSender(ctx, msg, text)
+                return seal.ext.newCmdExecuteResult(true);
             }
             case 'sort': {
                 if (!data[groupId].teams.hasOwnProperty(teamnow)) {
@@ -319,7 +311,7 @@ if (!ext) {
                 }
 
                 let text = seal.ext.getStringConfig(ext, "排序前缀语");
-                let ctxlst = data[groupId].teams[teamnow].map(mctxargs => { return getCtxById(...mctxargs); });
+                let ctxlst = data[groupId].teams[teamnow].map(userId => { return getCtxById(ctx.endPoint.userId, groupId, msg.guildId, userId); });
 
                 ctxlst.sort(function (a, b) { return seal.vars.intGet(b, val2)[0] - seal.vars.intGet(a, val2)[0]; });
                 for (let ctx of ctxlst) text += `\n${ctx.player.name} ${val2}${seal.vars.intGet(ctx, val2)[0]}`;
@@ -355,7 +347,7 @@ if (!ext) {
 
             let text = seal.ext.getStringConfig(ext, "呼叫队伍里所有成员前缀语")
             text = text.replace('{{时间限制}}', seal.ext.getIntConfig(ext, "呼叫时间限制（s）"))
-            for (let mctxargs of data[groupId].teams[teamnow]) text += `\n[CQ:at,qq=${mctxargs[3].replace(/\D+/g, "")}]`;
+            for (let userId of data[groupId].teams[teamnow]) text += `\n[CQ:at,qq=${userId.replace(/\D+/g, "")}]`;
             seal.replyToSender(ctx, msg, text)
 
             data[groupId].call = data[groupId].teams[teamnow]
@@ -364,7 +356,7 @@ if (!ext) {
                 text = text.replace('{{当前人数}}', data[groupId].teams[teamnow].length)
                 text = text.replace('{{签到人数}}', data[groupId].teams[teamnow].length - data[groupId].call.length)
                 text = text.replace('{{咕咕人数}}', data[groupId].call.length)
-                for (let mctxargs of data[groupId].call) text += `\n[CQ:at,qq=${mctxargs[3].replace(/\D+/g, "")}]`;
+                for (let userId of data[groupId].call) text += `\n[CQ:at,qq=${userId.replace(/\D+/g, "")}]`;
                 seal.replyToSender(ctx, msg, text)
                 return;
             }, seal.ext.getIntConfig(ext, "呼叫时间限制（s）") * 1000)
@@ -375,7 +367,7 @@ if (!ext) {
             if (!data.hasOwnProperty(groupId)) getData(groupId)
             if (data[groupId].call.length == 0) return;
 
-            data[groupId].call = data[groupId].call.filter(mctxargs => mctxargs[3] != ctx.player.userId)
+            data[groupId].call = data[groupId].call.filter(userId => userId != ctx.player.userId)
             return;
         }
         if (message == seal.ext.getStringConfig(ext, "开始战斗轮排序")) {
@@ -393,7 +385,7 @@ if (!ext) {
             }
 
             let text = seal.ext.getStringConfig(ext, "排序前缀语");
-            let ctxlst = data[groupId].teams[teamnow].map(mctxargs => { return getCtxById(...mctxargs); });
+            let ctxlst = data[groupId].teams[teamnow].map(userId => { return getCtxById(ctx.endPoint.userId, groupId, msg.guildId, userId); });
 
             ctxlst.sort(function (a, b) { return seal.vars.intGet(b, 'dex')[0] - seal.vars.intGet(a, 'dex')[0]; });
             for (let ctx of ctxlst) text += `\n${ctx.player.name} dex${seal.vars.intGet(ctx, 'dex')[0]}`;
