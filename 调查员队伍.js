75cf38989ecb27dbf10bd84e2e652fd48d65ba01
error2913 <2913949387@qq.com>
2024-10-13 21:46:07 +0800

    Update 调查员队伍.js

--- 调查员队伍.js
+++ 调查员队伍.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         team
 // @author       错误
-// @version      3.0.0
+// @version      3.1.0
 // @description  这是一个海豹插件，它提供了一套完整的队伍管理功能，允许用户在 QQ 群组中创建和管理队伍。\n- 更多自定义配置请查看配置项（即插件设置部分）。\n - 如果你也是一名插件作者，你也可以通过globalThis.team.xxx在你的插件中来调用该插件的方法。具体方法请在该插件的源码中查看。\n- 若使用过程中遇到问题或BUG，请联系开发者。如果您有更好的想法，欢迎前往主页提交 Pull Request 或 Issue，共同完善该插件
 // @timestamp    1724468302
 // 2024-08-24 10:58:22
@@ -13,26 +13,26 @@
 // 首先检查是否已经存在
 let ext = seal.ext.find('team');
 if (!ext) {
-    ext = seal.ext.new('team', '错误', '3.0.0');
+    ext = seal.ext.new('team', '错误', '3.1.0');
     seal.ext.register(ext);
     const data = {}
 
     seal.ext.registerTemplateConfig(ext, '新建队伍', ['新建<{{队伍名字}}>成功'])
     seal.ext.registerTemplateConfig(ext, '绑定队伍', ['绑定<{{队伍名字}}>成功'])
     seal.ext.registerTemplateConfig(ext, '删除队伍', ['删除<{{队伍名字}}>成功'])
-    seal.ext.registerTemplateConfig(ext, '展示队伍', ['队伍如下{{队伍列表}}'])
+    seal.ext.registerTemplateConfig(ext, '展示队伍', ['队伍如下：\n{{队伍列表}}'])
 
     seal.ext.registerTemplateConfig(ext, '添加成员', ['成功添加{{被@的长度}}位调查员，当前队伍人数{{当前人数}}人。'])
     seal.ext.registerTemplateConfig(ext, '删除成员', ['成功删除{{被@的长度}}位调查员，当前队伍人数{{当前人数}}人。'])
-    seal.ext.registerTemplateConfig(ext, '抽取成员', ['抽到了：{{成员名称}}'])
+    seal.ext.registerTemplateConfig(ext, '抽取成员', ['抽到了：\n{{成员名称}}'])
 
-    seal.ext.registerTemplateConfig(ext, '呼叫成员', ['下面的人来跑团啦！请在限定时间{{时间限制}}s内回复“到”：{{成员列表}}'])
-    seal.ext.registerTemplateConfig(ext, '呼叫结束', ['应到{{当前人数}}人，实到{{签到人数}}人，未到{{咕咕人数}}人。未到如下：{{成员列表}}'])
+    seal.ext.registerTemplateConfig(ext, '呼叫成员', ['下面的人来跑团啦！请在限定时间{{时间限制}}s内回复“到”：\n{{成员列表}}'])
+    seal.ext.registerTemplateConfig(ext, '呼叫结束', ['应到{{当前人数}}人，实到{{签到人数}}人，未到{{咕咕人数}}人。未到如下：\n{{成员列表}}'])
     seal.ext.registerIntConfig(ext, '呼叫时间限制（s）', 60)
 
-    seal.ext.registerTemplateConfig(ext, '展示属性', ['属性如下{{展示属性列表}}'])
-    seal.ext.registerTemplateConfig(ext, '修改属性', ['修改如下{{修改属性列表}}'])
-    seal.ext.registerTemplateConfig(ext, '排序属性', ['排序如下{{排序属性列表}}'])
+    seal.ext.registerTemplateConfig(ext, '展示属性', ['属性如下：\n{{展示属性列表}}'])
+    seal.ext.registerTemplateConfig(ext, '修改属性', ['修改如下：\n{{修改操作}}\n{{修改属性列表}}'])
+    seal.ext.registerTemplateConfig(ext, '排序属性', ['排序如下：\n{{排序属性列表}}'])
 
     seal.ext.registerTemplateConfig(ext, '提示队伍为空', ['队伍里没有成员。'])
     seal.ext.registerTemplateConfig(ext, '提示队伍不存在', ['队伍不存在。'])
@@ -122,6 +122,13 @@ if (!ext) {
                 let team = { name: name, members: [] }
                 teams.push(team)
                 data[groupId] = team;
+
+                //删除默认队伍
+                let defaultIndex = teams.findIndex(item => item.name === '默认队伍');
+                if (defaultIndex!== -1 && teams[defaultIndex].members.length === 0) {
+                    teams.splice(defaultIndex, 1);
+                }
+
                 var tmpl = seal.ext.getTemplateConfig(ext, "新建队伍")
             }
 
@@ -187,15 +194,14 @@ if (!ext) {
                 teams.push(team)
             }
 
-            let text = ''
-            teams.forEach(item => {
-                text += `\n${item.name} ${item.members.length}人`;
-                if (item.name == team.name) text += '◎'
+            let teamsText = teams.map(item => {
+                if (item.name == team.name) return `${item.name} ${item.members.length}人◎`;
+                else return `${item.name} ${item.members.length}人`;
             })
 
             const tmpl = seal.ext.getTemplateConfig(ext, "展示队伍")
             return tmpl[Math.floor(Math.random() * tmpl.length)]
-                .replace('{{队伍列表}}', text)
+                .replace('{{队伍列表}}', teamsText.join('\n'))
         }
 
 
@@ -228,7 +234,7 @@ if (!ext) {
                 .replace('{{当前人数}}', data[groupId].members.length);
         }
 
-        drawMember(groupId, epId, guildId) {
+        drawMember(groupId, num, epId, guildId) {
             const team = this.getBindData(groupId)
 
             if (team.members.length == 0) {
@@ -236,14 +242,22 @@ if (!ext) {
                 return tmpl[Math.floor(Math.random() * tmpl.length)]
             }
 
-            const members = team.members;
-            const userId = members[Math.floor(Math.random() * members.length)];
-            const mctx = getCtxById(epId, groupId, guildId, userId);
-            const name = mctx.player.name;
+            num = parseInt(num);
+            num = isNaN(num) ? 1 : num
+
+            let result = []
+            let copy = team.members.slice();
+
+            while (result.length < num && copy.length > 0) {
+                let randomIndex = Math.floor(Math.random() * copy.length);
+                const userId = copy.splice(randomIndex, 1)[0]
+                const mctx = getCtxById(epId, groupId, guildId, userId);
+                result.push(mctx.player.name);
+            }
 
             const tmpl = seal.ext.getTemplateConfig(ext, "抽取成员")
             return tmpl[Math.floor(Math.random() * tmpl.length)]
-                .replace('{{成员名称}}', name);
+                .replace('{{成员名称}}', result.join('\n'));
         }
 
         callMembers(groupId, epId, guildId) {
@@ -260,20 +274,16 @@ if (!ext) {
                 const tmpl = seal.ext.getTemplateConfig(ext, "提示正在呼叫");
                 return tmpl[Math.floor(Math.random() * tmpl.length)]
             }
-
-            const timeLimit = seal.ext.getIntConfig(ext, "呼叫时间限制（s）")
-            const tmpl = seal.ext.getTemplateConfig(ext, "呼叫成员")
-
-            let text = ''
-            for (let userId of team.members) text += `\n[CQ:at,qq=${userId.replace(/\D+/g, "")}]`;
             saveCallData(groupId, team.members);
 
+            const timeLimit = seal.ext.getIntConfig(ext, "呼叫时间限制（s）")
             setTimeout(() => {
                 const call = this.getCallData(groupId)
                 saveCallData(groupId, []);
 
-                let text = ''
-                for (let userId of call) text += `\n[CQ:at,qq=${userId.replace(/\D+/g, "")}]`;
+                let membersText = call.map(userId => {
+                    return `[CQ:at,qq=${userId.replace(/\D+/g, "")}]`;
+                });
 
                 const userId = 'QQ:114514'
                 const mctx = getCtxById(epId, groupId, guildId, userId)
@@ -288,11 +298,17 @@ if (!ext) {
                     .replace('{{当前人数}}', team.members.length)
                     .replace('{{签到人数}}', team.members.length - call.length)
                     .replace('{{咕咕人数}}', call.length)
-                    .replace('{{成员列表}}', text))
+                    .replace('{{成员列表}}', membersText.join('\n')));
             }, timeLimit * 1000);
+
+            let membersText = teams.members.map(userId => {
+                return `[CQ:at,qq=${userId.replace(/\D+/g, "")}]`;
+            });
+
+            const tmpl = seal.ext.getTemplateConfig(ext, "呼叫成员")
             return tmpl[Math.floor(Math.random() * tmpl.length)]
                 .replace('{{时间限制}}', timeLimit)
-                .replace('{{成员列表}}', text);
+                .replace('{{成员列表}}', membersText.join('\n'));
         }
 
         showAttrb(groupId, attrb, epId, guildId) {
@@ -303,23 +319,15 @@ if (!ext) {
                 return tmpl[Math.floor(Math.random() * tmpl.length)]
             }
 
-            let text = ''
-            if (!attrb) {
-                for (let userId of team.members) {
-                    let mctx = getCtxById(epId, groupId, guildId, userId);
-                    text += `\n${mctx.player.name} hp${seal.vars.intGet(mctx, 'hp')[0]}/${Math.floor((seal.vars.intGet(mctx, 'con')[0] + seal.vars.intGet(mctx, 'siz')[0]) / 10)} san${seal.vars.intGet(mctx, 'san')[0]}/${seal.vars.intGet(mctx, 'pow')[0]} dex${seal.vars.intGet(mctx, 'dex')[0]}`;
-                }
-            } else {
-                for (let userId of team.members) {
-                    let mctx = getCtxById(epId, groupId, guildId, userId);
-                    text += `\n${mctx.player.name} ${attrb}${seal.vars.intGet(mctx, attrb)[0]}`;
-                }
-            }
-
+            let membersText = team.members.map(userId => {
+                let mctx = getCtxById(epId, groupId, guildId, userId);
+                if(!attrb)return `${mctx.player.name} hp${seal.vars.intGet(mctx, 'hp')[0]}/${Math.floor((seal.vars.intGet(mctx, 'con')[0] + seal.vars.intGet(mctx, 'siz')[0]) / 10)} san${seal.vars.intGet(mctx, 'san')[0]}/${seal.vars.intGet(mctx, 'pow')[0]} dex${seal.vars.intGet(mctx, 'dex')[0]}`;
+                else return `${mctx.player.name} ${attrb}${seal.vars.intGet(mctx, attrb)[0]}`
+            });
 
             const tmpl = seal.ext.getTemplateConfig(ext, "展示属性");
             return tmpl[Math.floor(Math.random() * tmpl.length)]
-                .replace('{{展示属性列表}}', text);
+                .replace('{{展示属性列表}}', membersText.join('\n'));
         }
 
         setAttrb(groupId, val, epId, guildId) {
@@ -338,15 +346,16 @@ if (!ext) {
             let expression = match[2];
             let op = expression.match(/^([+\-])/)?.[1];
             let result = op ? `{${attrb}=${val}}` : `{${attrb}=${expression}}`;
-            let text = `\n操作:${val}`;
-            for (let userId of team.members) {
+            let opText = `操作:${val}`;
+            let membersText = team.members.map(userId => {
                 let mctx = getCtxById(epId, groupId, guildId, userId);
-                text += `\n${mctx.player.name} ${seal.vars.intGet(mctx, attrb)[0]}=>${seal.format(mctx, result)}`;
-            }
+                return `${mctx.player.name} ${seal.vars.intGet(mctx, attrb)[0]}=>${seal.format(mctx, result)}`;
+            });
 
             const tmpl = seal.ext.getTemplateConfig(ext, "修改属性");
             return tmpl[Math.floor(Math.random() * tmpl.length)]
-                .replace('{{修改属性列表}}', text)
+                .replace('{{修改操作}}', opText)
+                .replace('{{修改属性列表}}', membersText.join('\n'))
         }
 
         sortAttrb(groupId, attrb, epId, guildId) {
@@ -365,12 +374,13 @@ if (!ext) {
                 return seal.vars.intGet(b, attrb)[0] - seal.vars.intGet(a, attrb)[0];
             });
 
-            let text = ''
-            for (let ctx of ctxlst) text += `\n${ctx.player.name} ${attrb}${seal.vars.intGet(ctx, attrb)[0]}`;
+            let membersText = ctxlst.map(ctx => {
+                return `${ctx.player.name} ${attrb}${seal.vars.intGet(ctx, attrb)[0]}`;
+            });
 
             const tmpl = seal.ext.getTemplateConfig(ext, "排序属性");
             return tmpl[Math.floor(Math.random() * tmpl.length)]
-                .replace('{{排序属性列表}}', text);
+                .replace('{{排序属性列表}}', membersText.join('\n'));
         }
     }
 
@@ -412,7 +422,7 @@ if (!ext) {
 【.team add @xx@xxx...】添加若干成员
 【.team rm @xx@xxx...】删除若干成员
 【.team rm --all】删除所有成员
-【.team draw】随机抽取一个成员
+【.team draw (抽取数量)】随机抽取队伍内成员
 【.team call】调查员集结！
 【.team show (属性名)】查看属性
 【.team st <属性名><值/+-表达式>】修改属性
@@ -473,7 +483,7 @@ if (!ext) {
                 return seal.ext.newCmdExecuteResult(true);
             }
             case 'draw': {
-                seal.replyToSender(ctx, msg, team.drawMember(groupId, ctx.endPoint.userId, msg.guildId))
+                seal.replyToSender(ctx, msg, team.drawMember(groupId, val2, ctx.endPoint.userId, msg.guildId))
                 return seal.ext.newCmdExecuteResult(true);
             }
             case 'call': {
