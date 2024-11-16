933d808908e5165425af2c1839974e0ef99bbeec
error2913 <2913949387@qq.com>
2024-11-16 22:14:07 +0800

    Update 斗地主2.js

--- release/斗地主2.js
+++ release/斗地主2.js
@@ -19,10 +19,12 @@
     constructor() {
       this.name = "";
       this.desc = "";
-      this.type = "";
       this.cards = [];
-      this.data = [];
+      this.info = {
+        type: ""
+      };
       this.solve = (_, __, ___, ____) => {
+        return true;
       };
     }
     static parse(data) {
@@ -33,14 +35,17 @@
       try {
         if (deckMap.hasOwnProperty(data.name)) {
           const deck2 = deckMap[data.name].clone();
-          deck2.data = data.data || [];
+          for (const key in deck2.info) {
+            deck2.info[key] = data.info[key];
+          }
           return deck2;
         }
         deck.name = data.name;
         deck.desc = data.desc;
-        deck.type = data.type;
         deck.cards = data.cards;
-        deck.data = data.data;
+        for (const key in deck.info) {
+          deck.info[key] = data.info[key];
+        }
       } catch (err) {
         console.error(`解析牌组失败:`, err);
         deck.name = "未知牌堆";
@@ -59,13 +64,13 @@
       return this.cards.splice(position, n);
     }
     //在指定位置插入卡牌
-    add(cards2, position = 0) {
-      this.cards.splice(position, 0, ...cards2);
+    add(cards, position = 0) {
+      this.cards.splice(position, 0, ...cards);
     }
     //移除指定卡牌
-    remove(cards2) {
-      for (let i = 0; i < cards2.length; i++) {
-        const card = cards2[i];
+    remove(cards) {
+      for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
         const index = this.cards.indexOf(card);
         if (index !== -1) {
           this.cards.splice(index, 1);
@@ -73,10 +78,10 @@
       }
     }
     //检查是否包含指定卡牌
-    check(cards2) {
+    check(cards) {
       let copy = this.cards.slice();
-      for (let i = 0; i < cards2.length; i++) {
-        const card = cards2[i];
+      for (let i = 0; i < cards.length; i++) {
+        const card = cards[i];
         const index = copy.indexOf(card);
         if (index == -1) {
           return false;
@@ -90,9 +95,8 @@
       const deck = new _Deck();
       deck.name = this.name;
       deck.desc = this.desc;
-      deck.type = this.type;
       deck.cards = this.cards.slice();
-      deck.data = JSON.parse(JSON.stringify(this.data));
+      deck.info = JSON.parse(JSON.stringify(this.info));
       if (typeof this.solve === "function") {
         deck.solve = this.solve.bind(deck);
       }
@@ -100,79 +104,77 @@
     }
   };
   var deckMap = {};
-  var cards = [
-    "大王",
-    "小王",
-    "2",
-    "2",
-    "2",
-    "2",
-    "A",
-    "A",
-    "A",
-    "A",
-    "K",
-    "K",
-    "K",
-    "K",
-    "Q",
-    "Q",
-    "Q",
-    "Q",
-    "J",
-    "J",
-    "J",
-    "J",
-    "10",
-    "10",
-    "10",
-    "10",
-    "9",
-    "9",
-    "9",
-    "9",
-    "8",
-    "8",
-    "8",
-    "8",
-    "7",
-    "7",
-    "7",
-    "7",
-    "6",
-    "6",
-    "6",
-    "6",
-    "5",
-    "5",
-    "5",
-    "5",
-    "4",
-    "4",
-    "4",
-    "4",
-    "3",
-    "3",
-    "3",
-    "3"
-  ];
-  var deckMain = new Deck();
-  deckMain.name = "主牌堆";
-  deckMain.type = "public";
-  deckMain.cards = cards;
-  deckMap["主牌堆"] = deckMain;
-  var deckDiscard = new Deck();
-  deckDiscard.name = "弃牌堆";
-  deckDiscard.type = "public";
-  deckDiscard.cards = [];
-  deckMap["弃牌堆"] = deckDiscard;
+  function load() {
+    const cards = [
+      "大王",
+      "小王",
+      "2",
+      "2",
+      "2",
+      "2",
+      "A",
+      "A",
+      "A",
+      "A",
+      "K",
+      "K",
+      "K",
+      "K",
+      "Q",
+      "Q",
+      "Q",
+      "Q",
+      "J",
+      "J",
+      "J",
+      "J",
+      "10",
+      "10",
+      "10",
+      "10",
+      "9",
+      "9",
+      "9",
+      "9",
+      "8",
+      "8",
+      "8",
+      "8",
+      "7",
+      "7",
+      "7",
+      "7",
+      "6",
+      "6",
+      "6",
+      "6",
+      "5",
+      "5",
+      "5",
+      "5",
+      "4",
+      "4",
+      "4",
+      "4",
+      "3",
+      "3",
+      "3",
+      "3"
+    ];
+    const deckMain = new Deck();
+    deckMain.name = "主牌堆";
+    deckMain.cards = cards;
+    deckMap["主牌堆"] = deckMain;
+  }
 
   // src/player.ts
   var Player = class _Player {
     //暗牌
     constructor(id) {
       this.id = id;
-      this.data = [""];
+      this.info = {
+        class: ""
+      };
       this.hand = new Deck();
       this.hand.name = "手牌";
       this.show = new Deck();
@@ -189,7 +191,9 @@
       }
       try {
         player = new _Player(data.id);
-        player.data = data.data;
+        for (const key in player.info) {
+          player.info[key] = data.info[key];
+        }
         player.hand = Deck.parse(data.hand);
         player.show = Deck.parse(data.show);
         player.hide = Deck.parse(data.hide);
@@ -281,11 +285,11 @@
       if (n >= 5 && n <= 12) {
         const index = rank.indexOf(match[2]);
         if (index + n < 13) {
-          const cards2 = [];
+          const cards = [];
           for (let i = 0; i < n; i++) {
-            cards2.push(rank[index + i]);
+            cards.push(rank[index + i]);
           }
-          return [cards2, "顺", index];
+          return [cards, "顺", index];
         }
       }
     }
@@ -295,11 +299,11 @@
       if (n >= 3 && n <= 10) {
         const index = rank.indexOf(match[2]);
         if (index + n < 13) {
-          const cards2 = [];
+          const cards = [];
           for (let i = 0; i < n; i++) {
-            cards2.push(rank[index + i], rank[index + i]);
+            cards.push(rank[index + i], rank[index + i]);
           }
-          return [cards2, "连对", index];
+          return [cards, "连对", index];
         }
       }
     }
@@ -309,11 +313,11 @@
       if (n >= 2 && n <= 5) {
         const index = rank.indexOf(match[2]);
         if (index + n < 13) {
-          const cards2 = [];
+          const cards = [];
           for (let i = 0; i < n; i++) {
-            cards2.push(rank[index + i], rank[index + i], rank[index + i]);
+            cards.push(rank[index + i], rank[index + i], rank[index + i]);
           }
-          return [cards2, "飞机", index];
+          return [cards, "飞机", index];
         }
       }
     }
@@ -325,12 +329,12 @@
         if (match2 && match2.length == n) {
           const index = rank.indexOf(match[2]);
           if (index + n < 13) {
-            const cards2 = [];
+            const cards = [];
             for (let i = 0; i < n; i++) {
-              cards2.push(rank[index + i], rank[index + i], rank[index + i]);
+              cards.push(rank[index + i], rank[index + i], rank[index + i]);
             }
-            cards2.push(...match2);
-            return [cards2, "飞机带单张", index];
+            cards.push(...match2);
+            return [cards, "飞机带单张", index];
           }
         }
       }
@@ -343,12 +347,12 @@
         if (match2 && match2.length == n) {
           const index = rank.indexOf(match[2]);
           if (index + n < 13) {
-            const cards2 = [];
+            const cards = [];
             for (let i = 0; i < n; i++) {
-              cards2.push(rank[index + i], rank[index + i], rank[index + i]);
+              cards.push(rank[index + i], rank[index + i], rank[index + i]);
             }
-            cards2.push(...match2, ...match2);
-            return [cards2, "飞机带对子", index];
+            cards.push(...match2, ...match2);
+            return [cards, "飞机带对子", index];
           }
         }
       }
@@ -366,8 +370,12 @@
       this.players = [];
       this.round = 0;
       this.turn = 0;
-      this.curPlayerId = "";
-      this.curDeckInfo = ["", 0, ""];
+      this.info = {
+        id: "",
+        type: "",
+        value: 0,
+        deckId: ""
+      };
       this.mainDeck = deckMap["主牌堆"].clone();
     }
     static getData(ext, id) {
@@ -399,8 +407,9 @@
         game.players = data.players.map((player) => Player.parse(player));
         game.round = data.round;
         game.turn = data.turn;
-        game.curPlayerId = data.curPlayerId;
-        game.curDeckInfo = data.curDeckInfo;
+        for (const key in game.info) {
+          game.info[key] = data.info[key];
+        }
         game.mainDeck = Deck.parse(data.mainDeck);
       } catch (err) {
         console.error("解析游戏数据失败:", err);
@@ -432,20 +441,19 @@ ${this.players[index].hand.cards.join("\n")}`);
         const j = Math.floor(Math.random() * (i + 1));
         [this.players[i], this.players[j]] = [this.players[j], this.players[i]];
       }
-      this.players[0].data[0] = "地主";
-      this.players[1].data[0] = "农民";
-      this.players[2].data[0] = "农民";
+      this.players[0].info.class = "地主";
+      this.players[1].info.class = "农民";
+      this.players[2].info.class = "农民";
       this.status = true;
-      this.curDeckInfo[2] = this.players[0].id;
       this.mainDeck.shuffle();
-      const cards2 = this.mainDeck.cards.splice(0, 3);
-      this.players[0].hand.add(cards2);
+      const cards = this.mainDeck.cards.splice(0, 3);
+      this.players[0].hand.add(cards);
       seal.replyToSender(ctx, msg, `地主的底牌为：
-${cards2.join("\n")}`);
+${cards.join("\n")}`);
       for (let i = 0; i < this.players.length; i++) {
         const player = this.players[i];
-        const cards3 = this.mainDeck.draw(0, 17);
-        player.hand.add(cards3);
+        const cards2 = this.mainDeck.draw(0, 17);
+        player.hand.add(cards2);
         const ranks = ["3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2", "小王", "大王"];
         player.hand.cards.sort((a, b) => {
           const indexA = ranks.indexOf(a);
@@ -455,6 +463,8 @@ ${cards2.join("\n")}`);
         replyPrivate(ctx, `您的手牌为:
 ${player.hand.cards.join("\n")}`, player.id);
       }
+      this.info.deckId = this.players[0].id;
+      this.info.id = this.players[0].id;
       const name = getName(ctx, this.players[0].id);
       seal.replyToSender(ctx, msg, `游戏开始，从地主${name}开始`);
       this.nextRound(ctx, msg);
@@ -473,31 +483,31 @@ ${player.hand.cards.join("\n")}`, player.id);
     //进入下一轮
     nextTurn(ctx, msg) {
       if (this.turn == 0) {
-        this.curPlayerId = this.players[0].id;
+        this.info.id = this.players[0].id;
       } else {
-        const index = this.players.findIndex((player) => player.id === this.curPlayerId);
+        const index = this.players.findIndex((player) => player.id === this.info.id);
         if (index == this.players.length - 1) {
           this.nextRound(ctx, msg);
           return;
         }
-        this.curPlayerId = this.players[index + 1].id;
+        this.info.id = this.players[index + 1].id;
       }
       this.turn++;
     }
     play(ctx, msg, cmdArgs) {
       const name = cmdArgs.getArgN(1).toUpperCase();
-      if (ctx.player.userId !== this.curPlayerId) {
+      if (ctx.player.userId !== this.info.id) {
         seal.replyToSender(ctx, msg, "不是当前玩家");
         return;
       }
-      const index = this.players.findIndex((player2) => player2.id === this.curPlayerId);
+      const index = this.players.findIndex((player2) => player2.id === this.info.id);
       const player = this.players[index];
-      const playerName = getName(ctx, this.curPlayerId);
+      const playerName = getName(ctx, this.info.id);
       const anotherIndex = index < this.players.length - 1 ? index + 1 : 0;
       const anotherPlayer = this.players[anotherIndex];
       const anotherName = getName(ctx, anotherPlayer.id);
       if (name == "SKIP" || name == "PASS" || name == "不要" || name == "要不起" || name == "过" || name == "不出") {
-        if (this.curDeckInfo[2] == this.curPlayerId) {
+        if (this.info.deckId == this.info.id) {
           seal.replyToSender(ctx, msg, "不能跳过");
           return;
         }
@@ -505,29 +515,34 @@ ${player.hand.cards.join("\n")}`, player.id);
         this.nextTurn(ctx, msg);
         return;
       }
-      const [cards2, type, value] = getCards(name);
+      const [cards, type, value] = getCards(name);
       if (!type) {
         seal.replyToSender(ctx, msg, "不存在牌型");
         return;
       }
-      if (!player.hand.check(cards2)) {
+      if (!player.hand.check(cards)) {
         seal.replyToSender(ctx, msg, "手牌不足");
         return;
       }
-      if (this.curDeckInfo[2] !== this.curPlayerId && this.curDeckInfo) {
-        if (type !== "炸弹" && type !== this.curDeckInfo[0]) {
+      if (this.info.deckId !== this.info.id && this.info) {
+        if (type !== "炸弹" && type !== this.info.type) {
           seal.replyToSender(ctx, msg, "牌型错误");
           return;
         }
-        if (type == this.curDeckInfo[0] && value <= this.curDeckInfo[1]) {
+        if (type == this.info.type && value <= this.info.value) {
           seal.replyToSender(ctx, msg, "牌不够大");
           return;
         }
       }
-      player.hand.remove(cards2);
-      this.curDeckInfo = [type, value, this.curPlayerId];
+      player.hand.remove(cards);
+      this.info = {
+        id: this.info.id,
+        type,
+        value,
+        deckId: this.info.id
+      };
       if (player.hand.cards.length == 0) {
-        seal.replyToSender(ctx, msg, `${player.data[0]}${playerName}胜利了`);
+        seal.replyToSender(ctx, msg, `${player.info.class}${playerName}胜利了`);
         this.end(ctx, msg);
         return;
       }
@@ -549,7 +564,7 @@ ${player.hand.cards.join("\n")}`, player.id);
     const cmdPlay = seal.ext.newCmdItemInfo();
     cmdPlay.name = "ddz";
     cmdPlay.help = `帮助：
-【.ddz start】
+【.ddz start】使用team里的成员创建游戏
 【.ddz end】
 【.ddz check】查看手牌
 【.ddz test 牌型名称】测试牌型是否存在
@@ -567,6 +582,10 @@ n飞机x带yz...、n飞机x带对yz...
 3334445556667778910AJ`;
     cmdPlay.disabledInPrivate = true;
     cmdPlay.solve = (ctx, msg, cmdArgs) => {
+      if (Object.keys(deckMap).length === 0) {
+        console.log("开始加载牌组");
+        load();
+      }
       let val = cmdArgs.getArgN(1);
       const id = ctx.group.groupId;
       switch (val) {
@@ -584,9 +603,9 @@ n飞机x带yz...、n飞机x带对yz...
         }
         case "test": {
           const s = cmdArgs.getRestArgsFrom(2);
-          const [cards2, type, value] = getCards(s);
+          const [cards, type, value] = getCards(s);
           if (type) {
-            seal.replyToSender(ctx, msg, `${cards2.join("")}存在${type}:${value}`);
+            seal.replyToSender(ctx, msg, `${cards.join("")}存在${type}:${value}`);
             return seal.ext.newCmdExecuteResult(true);
           }
           seal.replyToSender(ctx, msg, "不存在");
