263b54dc541d3887c4f193cbdeb0008dba479dde
error2913 <2913949387@qq.com>
2025-01-27 23:07:14 +0800

    Update redbag.js
    调整url构建过程

--- redbag.js
+++ redbag.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         豹语变量红包
 // @author       错误
-// @version      1.1.0
+// @version      1.1.1
 // @description  在插件设置填入红包使用货币的豹语变量
 // @timestamp    1735226264
 // 2024-12-26 23:17:44
@@ -13,7 +13,7 @@
 
 let ext = seal.ext.find('redbag');
 if (!ext) {
-    ext = seal.ext.new('redbag', '错误', '1.1.0');
+    ext = seal.ext.new('redbag', '错误', '1.1.1');
     seal.ext.register(ext);
 }
 
@@ -133,7 +133,9 @@ cmdSend.solve = (ctx, msg, cmdArgs) => {
         }
 
         sendRedbag(epId, gid, uid, un, amount, 1, muid, mun);
-        seal.replyToSender(ctx, msg, `[CQ:image,file=${url}/send_exclusive_redbag?user_id=${uid.replace(/\D+/g, '')}&user_name=${un.replace(/\]/g, '%5D').replace(/,/g, '%2C')}&target_user_id=${muid.replace(/\D+/g, '')}&target_user_name=${mun.replace(/\]/g, '%5D').replace(/,/g, '%2C')}&amount=${amount}&text=${text ? text.replace(/\]/g, '%5D').replace(/,/g, '%2C') : '恭喜发财，大吉大利'}]`);
+
+        const file = `${url}/send_exclusive_redbag?user_id=${uid.replace(/\D+/g, '')}&user_name=${un}&target_user_id=${muid.replace(/\D+/g, '')}&target_user_name=${mun}&amount=${amount}&text=${text ? text : '恭喜发财，大吉大利'}`;
+        seal.replyToSender(ctx, msg, `[CQ:image,file=${file.replace(/\]/g, '%5D').replace(/,/g, '%2C')}]`);
     } else {
         const total = parseInt(cmdArgs.getArgN(2));
         if (isNaN(total) || total < 1) {
@@ -157,7 +159,9 @@ cmdSend.solve = (ctx, msg, cmdArgs) => {
             return seal.ext.newCmdExecuteResult(true);
         }
         sendRedbag(epId, gid, uid, un, amount, total, '', '');
-        seal.replyToSender(ctx, msg, `[CQ:image,file=${url}/send_redbag?user_id=${uid.replace(/\D+/g, '')}&user_name=${un.replace(/\]/g, '%5D').replace(/,/g, '%2C')}&amount=${amount}&total=${total}&text=${text ? text.replace(/\]/g, '%5D').replace(/,/g, '%2C') : '恭喜发财，大吉大利'}]`);
+
+        const file = `${url}/send_redbag?user_id=${uid.replace(/\D+/g, '')}&user_name=${un}&amount=${amount}&total=${total}&text=${text ? text : '恭喜发财，大吉大利'}`;
+        seal.replyToSender(ctx, msg, `[CQ:image,file=${file.replace(/\]/g, '%5D').replace(/,/g, '%2C')}]`);
     }
 
     seal.vars.intSet(ctx, varname, val - amount);
@@ -239,7 +243,9 @@ cmdGrab.solve = (ctx, msg, cmdArgs) => {
             for (let i = 0; i < history.length; i++) {
                 remaining -= history[i].amount;
             }
-            seal.replyToSender(ctx, msg, `[CQ:image,file=${url}/open_redbag?user_id=${uid.replace(/\D+/g, '')}&user_name=${un.replace(/\]/g, '%5D').replace(/,/g, '%2C')}&sender_user_name=${data[gid][index].un.replace(/\]/g, '%5D').replace(/,/g, '%2C')}&amount=${increase}&total=${total}&remaining=${remaining}&history=${JSON.stringify(history).replace(/\]/g, '%5D').replace(/,/g, '%2C')}]`);
+
+            const file = `${url}/open_redbag?user_id=${uid.replace(/\D+/g, '')}&user_name=${un}&sender_user_name=${data[gid][index].un}&amount=${increase}&total=${total}&remaining=${remaining}&history=${JSON.stringify(history)}`;
+            seal.replyToSender(ctx, msg, `[CQ:image,file=${file.replace(/\]/g, '%5D').replace(/,/g, '%2C')}]`);
 
             if (history.length === total) {
                 data[gid].splice(index, 1);
@@ -294,7 +300,8 @@ cmdShow.solve = (ctx, msg, cmdArgs) => {
                 remaining -= history[i].amount;
             }
 
-            s += `[CQ:image,file=${url}/history?total=${total}&remaining=${remaining}&history=${JSON.stringify(history).replace(/\]/g, '%5D').replace(/,/g, '%2C')}]`;
+            const file = `${url}/history?total=${total}&remaining=${remaining}&history=${JSON.stringify(history)}`;
+            s += `[CQ:image,file=${file.replace(/\]/g, '%5D').replace(/,/g, '%2C')}]`;
         } else {
             s += `\n数量:${total}`;
         }
@@ -340,7 +347,8 @@ seal.ext.registerTask(ext, "cron", "0 */2 * * *", async () => {
                         remaining -= history[i].amount;
                     }
 
-                    s += `[CQ:image,file=${url}/history?total=${total}&remaining=${remaining}&history=${JSON.stringify(history).replace(/\]/g, '%5D').replace(/,/g, '%2C')}]`;
+                    const file = `${url}/history?total=${total}&remaining=${remaining}&history=${JSON.stringify(history)}`;
+                    s += `[CQ:image,file=${file.replace(/\]/g, '%5D').replace(/,/g, '%2C')}]`;
                 } else {
                     s += `\n数量:${total}`;
                 }
@@ -405,7 +413,9 @@ ext.onNotCommandReceived = (ctx, msg) => {
         for (let i = 0; i < history.length; i++) {
             remaining -= history[i].amount;
         }
-        seal.replyToSender(ctx, msg, `[CQ:image,file=${url}/open_redbag?user_id=${uid.replace(/\D+/g, '')}&user_name=${un.replace(/\]/g, '%5D').replace(/,/g, '%2C')}&sender_user_name=${data[gid][index].un.replace(/\]/g, '%5D').replace(/,/g, '%2C')}&amount=${increase}&total=${total}&remaining=${remaining}&history=${JSON.stringify(history).replace(/\]/g, '%5D').replace(/,/g, '%2C')}]`);
+
+        const file = `${url}/open_redbag?user_id=${uid.replace(/\D+/g, '')}&user_name=${un}&sender_user_name=${data[gid][index].un}&amount=${increase}&total=${total}&remaining=${remaining}&history=${JSON.stringify(history)}`;
+        seal.replyToSender(ctx, msg, `[CQ:image,file=${file.replace(/\]/g, '%5D').replace(/,/g, '%2C')}]`);
 
         if (history.length === total) {
             data[gid].splice(index, 1);
