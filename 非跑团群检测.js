420a0a4413e9f0658d5bae145db44760320d7fc8
error2913 <2913949387@qq.com>
2025-11-05 23:56:20 +0800

    Update 非跑团群检测.js

--- 非跑团群检测.js
+++ 非跑团群检测.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         非跑团群检测
 // @author       错误
-// @version      1.0.0
-// @description  请在插件设置填写相关配置。插件原理为：使用跑团功能超过阈值时间只使用娱乐功能则告警
+// @version      1.0.1
+// @description  请在插件设置填写相关配置。插件原理为：进群后或使用跑团功能后，超过阈值时间，使用了娱乐功能，则会向通知列表告警\n注：私聊或bot off时不会检测
 // @timestamp    1762271196
 // 2025-11-04 23:46:36
 // @license      MIT
@@ -13,29 +13,50 @@
 
 let ext = seal.ext.find('非跑团群检测');
 if (!ext) {
-    ext = seal.ext.new('非跑团群检测', '错误', '1.0.0');
+    ext = seal.ext.new('非跑团群检测', '错误', '1.0.1');
     seal.ext.register(ext);
 }
 
+seal.ext.registerTemplateConfig(ext, '白名单群号', [''], '');
 seal.ext.registerTemplateConfig(ext, '跑团指令', ['r', 'ra', 'rc', 'rh', 'sc', 'ri', 'log', 'setcoc', 'coc', 'dnd'], '');
 seal.ext.registerTemplateConfig(ext, '跑团非指令正则表达式', [''], '');
 seal.ext.registerTemplateConfig(ext, '娱乐指令', ['jrrp'], '');
 seal.ext.registerTemplateConfig(ext, '娱乐非指令正则表达式', [''], '');
-seal.ext.registerFloatConfig(ext, '告警阈值时间/h', 12, '使用跑团功能超过阈值时间只使用娱乐功能则告警');
-seal.ext.registerFloatConfig(ext, '告警冷却时间/h', 1, '');
+seal.ext.registerFloatConfig(ext, '告警阈值时间/h', 12, '');
+seal.ext.registerFloatConfig(ext, '告警冷却时间/h', 4, '');
 
 const data = JSON.parse(ext.storageGet('data') || '{}');
 const coolDog = JSON.parse(ext.storageGet('coolDog') || '{}');
 
-ext.onCommandReceived = (ctx, msg, cmdArgs) => {
-    if (ctx.isPrivate) return;
-    const gid = ctx.group.groupId;
-
-    const trpgCmd = seal.ext.getTemplateConfig(ext, '跑团指令');
-    if (trpgCmd.includes(cmdArgs.command)) {
-        data[gid] = msg.time;
-        ext.storageSet('data', JSON.stringify(data));
+function getRegexConfig(key) {
+    const regexes = seal.ext.getTemplateConfig(ext, key).filter(x => x);
+    if (regexes.length > 0) {
+        try {
+            return new RegExp(regexes.join('|'));
+        } catch (e) {
+            console.error(`正则表达式错误，内容:${regexes.join('|')}，错误信息:${e.message}`);
+            return /[^]/;
+        }
     }
+    return /[^]/;
+}
+
+function checkMsg(ctx) {
+    if (ctx.isPrivate || !ctx.group.active) return false;
+    const gid = ctx.group.groupId;
+    const whiteList = seal.ext.getTemplateConfig(ext, '白名单群号');
+    if (whiteList.includes(gid.replace(/^.+:/, ''))) return false;
+    return true;
+}
+
+function handleTrpgMsg(ctx, msg) {
+    const gid = ctx.group.groupId;
+    data[gid] = msg.time;
+    ext.storageSet('data', JSON.stringify(data));
+}
+
+function handlePlayMsg(ctx, msg) {
+    const gid = ctx.group.groupId;
 
     const cooldownInterval = seal.ext.getFloatConfig(ext, '告警冷却时间/h');
     if (coolDog[gid]) {
@@ -44,59 +65,28 @@ ext.onCommandReceived = (ctx, msg, cmdArgs) => {
         ext.storageSet('coolDog', JSON.stringify(coolDog));
     }
 
-    const playCmd = seal.ext.getTemplateConfig(ext, '娱乐指令');
-    if (playCmd.includes(cmdArgs.command)) {
-        const lastTime = data[gid] || ctx.group.enteredTime;
-        const alertThreshold = seal.ext.getFloatConfig(ext, '告警阈值时间/h');
-        if (msg.time - lastTime > alertThreshold * 60) {
-            coolDog[gid] = msg.time;
-            ext.storageSet('coolDog', JSON.stringify(coolDog));
-            ctx.notice(`群聊(${gid})使用了娱乐功能，超过阈值时间, 请检查是否为非跑团群\n检测指令:${cmdArgs.command}`);
-        }
+    const lastTime = data[gid] || ctx.group.enteredTime;
+    const alertThreshold = seal.ext.getFloatConfig(ext, '告警阈值时间/h');
+    if (alertThreshold <= 0) return;
+    if (msg.time - lastTime > alertThreshold * 60) {
+        coolDog[gid] = msg.time;
+        ext.storageSet('coolDog', JSON.stringify(coolDog));
+        ctx.notice(`群聊(${gid})使用了娱乐功能，超过阈值时间, 请检查是否为非跑团群\n检测消息:${msg.message}`);
     }
 }
 
+ext.onCommandReceived = (ctx, msg, cmdArgs) => {
+    if (!checkMsg(ctx)) return;
+    const trpgCmd = seal.ext.getTemplateConfig(ext, '跑团指令');
+    if (trpgCmd.includes(cmdArgs.command)) handleTrpgMsg(ctx, msg);
+    const playCmd = seal.ext.getTemplateConfig(ext, '娱乐指令');
+    if (playCmd.includes(cmdArgs.command)) handlePlayMsg(ctx, msg);
+}
+
 ext.onNotCommandReceived = (ctx, msg) => {
-    if (ctx.isPrivate) return;
-    const gid = ctx.group.groupId;
-
-    const trpgRegexes = seal.ext.getTemplateConfig(ext, '跑团非指令正则表达式').filter(x => x);
-    let trpgPattern;
-    if (trpgRegexes.length > 0) {
-        try {
-            trpgPattern = new RegExp(trpgRegexes.join('|'));
-        } catch (e) {
-            console.error(`正则表达式错误，内容:${trpgRegexes.join('|')}，错误信息:${e.message}`);
-        }
-    }
-    if (trpgPattern && trpgPattern.test(msg.message)) {
-        data[gid] = msg.time;
-        ext.storageSet('data', JSON.stringify(data));
-    }
-
-    const cooldownInterval = seal.ext.getFloatConfig(ext, '告警冷却时间/h');
-    if (coolDog[gid]) {
-        if (msg.time - coolDog[gid] < cooldownInterval * 60) return;
-        delete coolDog[gid];
-        ext.storageSet('coolDog', JSON.stringify(coolDog));
-    }
-
-    const playRegexes = seal.ext.getTemplateConfig(ext, '娱乐非指令正则表达式').filter(x => x);
-    let playPattern;
-    if (playRegexes.length > 0) {
-        try {
-            playPattern = new RegExp(playRegexes.join('|'));
-        } catch (e) {
-            console.error(`正则表达式错误，内容:${playRegexes.join('|')}，错误信息:${e.message}`);
-        }
-    }
-    if (playPattern && playPattern.test(msg.message)) {
-        const lastTime = data[gid] || ctx.group.enteredTime;
-        const alertThreshold = seal.ext.getFloatConfig(ext, '告警阈值时间/h');
-        if (msg.time - lastTime > alertThreshold * 60) {
-            coolDog[gid] = msg.time;
-            ext.storageSet('coolDog', JSON.stringify(coolDog));
-            ctx.notice(`群聊(${gid})使用了娱乐功能，超过阈值时间, 请检查是否为非跑团群\n检测消息:${msg.message}`);
-        }
-    }
+    if (!checkMsg(ctx)) return;
+    const trpgRegex = getRegexConfig('跑团非指令正则表达式');
+    if (trpgRegex.test(msg.message)) handleTrpgMsg(ctx, msg);
+    const playRegex = getRegexConfig('娱乐非指令正则表达式');
+    if (playRegex.test(msg.message)) handlePlayMsg(ctx, msg);
 }
